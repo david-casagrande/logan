728b576647b32734f07fea68d9f0283eb736de89
David Casagrande <dncasagrande@gmail.com>
2015-05-04 11:57:02 -0400

    started integrating custom fonts

--- functions/enque_styles.php
+++ functions/enque_styles.php
@@ -3,8 +3,8 @@
     $template_dir = get_template_directory_uri();
     $css_file = (defined('PRODUCTION') && constant('PRODUCTION')) ? '/css/logan-sausage.min.css' : '/css/logan-sausage.css';
 
-    // wp_register_style('google_font', 'https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,300,400');
-    // wp_enqueue_style('google_font');
+    wp_register_style('google_font', 'https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic');
+    wp_enqueue_style('google_font');
     wp_register_style('logan_sausage', $template_dir.$css_file);
     wp_enqueue_style('logan_sausage');
   }
