e2b58dac6b97c57c0383c7f5edf642227fd6d4b7
David Casagrande <dncasagrande@gmail.com>
2015-05-14 00:09:06 -0400

    updates

--- archive-product.php
+++ archive-product.php
@@ -1,22 +1,22 @@
 <?php get_header(); ?>
 
 <?php
-      $navOpts = array(
-        'theme_location'  => 'product-nav',
-        'container'       => '',
-        'items_wrap'      => '<ul id="%1$s" class="%2$s sub-nav">%3$s</ul>'
-      );
-      $image = get_theme_mod('products_image');
-      echo "<div class=\"main-view\">";
-        echo "<div class=\"row extra-padding\">";
-          echo "<div class=\"medium-3 columns\">";
-            wp_nav_menu($navOpts);
-          echo "</div>";
-          echo "<div class=\"medium-9 columns narrative\">";
-            echo "<p><img src=\"{$image}\" alt=\"{$title}\" /></p>";
-          echo "</div>";
-        echo "</div>";
+  $navOpts = array(
+    'theme_location'  => 'product-nav',
+    'container'       => '',
+    'items_wrap'      => '<ul id="%1$s" class="%2$s sub-nav">%3$s</ul>'
+  );
+  $image = get_theme_mod('products_image');
+  echo "<div class=\"main-view\">";
+    echo "<div class=\"row extra-padding\">";
+      echo "<div class=\"medium-3 columns\">";
+        wp_nav_menu($navOpts);
       echo "</div>";
+      echo "<div class=\"medium-9 columns narrative\">";
+        echo "<p><img src=\"{$image}\" alt=\"{$title}\" /></p>";
+      echo "</div>";
+    echo "</div>";
+  echo "</div>";
 ?>
 
 <?php get_footer(); ?>
