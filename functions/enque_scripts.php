a83a53948abca3e9494209304a26775a481db9c9
David Casagrande <dncasagrande@gmail.com>
2015-06-17 22:22:15 -0400

    use correct path to js

--- functions/enque_scripts.php
+++ functions/enque_scripts.php
@@ -1,7 +1,7 @@
 <?php
   function register_scripts() {
     $template_dir = get_template_directory_uri();
-    $js_file = (defined('PRODUCTION') && constant('PRODUCTION')) ? '/javascript/dist/logan-sausage.min.js' : '/javascript/logan-sausage.js';
+    $js_file = (defined('PRODUCTION') && constant('PRODUCTION')) ? '/javascript/dist/logan-sausage.js' : '/javascript/logan-sausage.js';
     $vendor = (defined('PRODUCTION') && constant('PRODUCTION')) ? '/javascript/dist/vendor.js' : '/javascript/vendor.js';
 
     wp_register_script('logan_sausage', $template_dir.$js_file, ['vendor'], '0.0.1', true);
