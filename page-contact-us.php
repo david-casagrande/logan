e53063872fb60e31149504eb2e329a3e0ce0dd9d
David Casagrande <dncasagrande@gmail.com>
2015-06-29 11:57:24 -0400

    added new section to contact page

--- page-contact-us.php
+++ page-contact-us.php
@@ -8,28 +8,32 @@
       $heading = get_field('heading');
       $narrative1 = get_field('narrative_1');
       $narrative2 = get_field('narrative_2');
+      $narrative3 = get_field('narrative_3');
+      $email = get_field('email');
       $twitter = get_field('twitter');
       $facebook = get_field('facebook');
 
       echo "<div class=\"main-view\">";
         echo "<div id=\"contact-us\" class=\"row extra-padding\">";
           echo "<div class=\"medium-6 columns narrative\"><p><img src=\"{$image}\" alt=\"Contact Us\" /></p></div>";
-          echo "<div class=\"medium-5 columns\">";
+          echo "<div class=\"medium-6 columns\">";
             echo "<div class=\"row\">";
               echo "<div class=\"small-12 columns narrative\">{$heading}</div>";
               echo "<div class=\"medium-6 columns narrative\">{$narrative1}</div>";
               echo "<div class=\"medium-6 columns narrative\">{$narrative2}</div>";
-              echo "<div class=\"small-12 columns narrative\">";
+              echo "<div class=\"medium-6 columns narrative\">{$narrative3}</div>";
+              echo "<div class=\"medium-6 columns narrative\">";
                 echo "<h3 class=\"with-margin\">Stay Connected</h3>";
                 echo "<a href=\"http://twitter.com/{$twitter}\" class=\"social-media\" target=\"_blank\">";
                   echo "<i class=\"fa fa-twitter-square\"></i><span>{$twitter}</span>";
                 echo "</a>";
-              echo "</div>";
-              echo "<div class=\"small-12 columns narrative\">";
+              // echo "</div>";
+              // echo "<div class=\"small-12 columns narrative\">";
                 echo "<a href=\"http://facebook.com/{$facebook}\" class=\"social-media\" target=\"_blank\">";
                   echo "<i class=\"fa fa-facebook-official\"></i><span>{$facebook}</span>";
                 echo "</a>";
               echo "</div>";
+
             echo "</div>";
           echo "</div>";
         echo "</div>";
